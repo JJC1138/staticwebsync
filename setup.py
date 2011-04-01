0ceb4aaeaf809e7a8eb12dc21ddb2e7eacc77437
Jon Colverson <staticwebsyncgit@vcxz.co.uk>
2011-04-01 22:40:52 +0100

    Added basic metadata to setup.py.

--- setup.py
+++ setup.py
@@ -1,20 +1,36 @@
 from distutils.core import setup
-from glob import glob
+import sys
 
-import py2exe
+extra_options = {}
+if 'py2exe' in sys.argv:
+    from glob import glob
+    import py2exe
+    extra_options.update(
+        console=['bin/sws.py'],
+        zipfile=None,
+        options={'py2exe': {
+            'bundle_files': 1,
+            'packages': ['email'],
+            'dll_excludes': ['w9xpopen.exe'],
+            'ignores': ['_scproxy', 'email.Encoders', 'email.MIMEBase',
+                'email.MIMEMultipart', 'email.MIMEText', 'email.Utils', 'lxml',
+                'netbios', 'simplejson', 'win32evtlog', 'win32evtlogutil',
+                'win32wnet'],
+        }},
+        data_files=[('Microsoft.VC90.CRT', glob(
+            r'C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))],
+    )
 
 setup(
-    console=['sws.py'],
-    zipfile=None,
-    options={'py2exe': {
-        'bundle_files': 1,
-        'packages': ['email'],
-        'dll_excludes': ['w9xpopen.exe'],
-        'ignores': ['_scproxy', 'email.Encoders', 'email.MIMEBase',
-            'email.MIMEMultipart', 'email.MIMEText', 'email.Utils', 'lxml',
-            'netbios', 'simplejson', 'win32evtlog', 'win32evtlogutil',
-            'win32wnet'],
-    }},
-    data_files=[('Microsoft.VC90.CRT', glob(
-        r'C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))],
+    name='staticwebsync',
+    version='1.0.0dev',
+    maintainer='Jon Colverson',
+    maintainer_email='staticwebsync@jjc1138.net',
+    packages=['staticwebsync'],
+    scripts=['bin/sws.py'],
+    url='http://staticwebsync.com/',
+    license='MIT',
+    description='Automates setting up S3/CloudFront for web site hosting',
+    long_description=open('README.txt').read(),
+    **extra_options
 )
